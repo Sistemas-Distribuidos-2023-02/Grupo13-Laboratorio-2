803b714a770b47d62bc0bf12217aa6e1098e4205
ScaryShiroyasha <diego.acevedos@usm.cl>
2023-10-18 19:48:06 -0300

    funcion no implementada

--- continentes/asia/main.go
+++ continentes/asia/main.go
@@ -1,88 +1,115 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"log"
+	"math/rand"
+	"os"
 	"time"
 
+	pb "github.com/VicenteRuizA/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	pb "github.com/VicenteRuizA/proto"
 )
 
 const (
-	defaultname = "Cristiano Ronaldo"
+	defaultname      = "Cristiano Ronaldo"
 	defaultcondition = "INFECTADO"
 )
 
 var (
 
-    // NOTAR: se asume servidor en vm 50, de ahi la ip:puerto
-    // ip se establece aqui, puerto en servidor.
-	addr = flag.String("addr", "10.6.46.60:50051", "ip address to connect to")
-	name = flag.String("name", defaultname, "Name to report")
+	// NOTAR: se asume servidor en vm 50, de ahi la ip:puerto
+	// ip se establece aqui, puerto en servidor.
+	//addr = flag.String("addr", "10.6.46.60:50051", "ip address to connect to")
+	addr      = flag.String("addr", "localhost:50051", "ip address to connect to")
+	name      = flag.String("name", defaultname, "Name to report")
 	condition = flag.String("condition", defaultcondition, "Condition to report")
 )
 
+func readNamesFromFile(filename string, numNames int) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var names []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		names = append(names, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(names), func(i, j int) {
+		names[i], names[j] = names[j], names[i]
+	})
+
+	return names[:numNames]
+}
 
 func main() {
 	// Asignar variables si es que existe flag al compilar
 	flag.Parse()
 
-	
-    // Crear connection por el mismo puerto del listener del servidor
-    conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil{
+	// Crear connection por el mismo puerto del listener del servidor
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
 		log.Fatalf("fallo la conexion: %v", err)
 	}
 	defer conn.Close()
 
-	/* 
-	Utilizando los compilados de protocol buffer generar un cliente que
-	pida el servicio Report definido en message.proto
-	hacia el servidor con el cual se establecio la conexion conn. 
+	/*
+		Utilizando los compilados de protocol buffer generar un cliente que
+		pida el servicio Report definido en message.proto
+		hacia el servidor con el cual se establecio la conexion conn.
 	*/
 	c := pb.NewReportClient(conn)
 
 	/* Generar contexto
 	Los contextos nos permiten compartir informacion entre distintos
-	ambientes, en este caso el ambiente donde corre el cliente y el 
-	ambiente del servidor. El efecto de este codigo segun entiendo son 
+	ambientes, en este caso el ambiente donde corre el cliente y el
+	ambiente del servidor. El efecto de este codigo segun entiendo son
 	los tiempos de ejecuccion presentes al ejecutarse tanto el cliente
-	como el servidor. Ambos muestran su propio tiempo o context permite 
-	que ambos muestren tiempo del cliente? Sino, que informacion comparte 
+	como el servidor. Ambos muestran su propio tiempo o context permite
+	que ambos muestren tiempo del cliente? Sino, que informacion comparte
 	este context?
 	*/
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	
-	/*
-	Se realiza el request a traves de la conexion 
-	Notar que al compilar el .proto se crean structs de SeverityRequest y SeverityReply
-	en message.pb.go, dicho archivo trata con las estructuras de datos y la serializacion, es decir,
-	el ensamblado tangible de los datos para la comunicacion.
 
-	Por otro lado message_grpc.pb.gp trata con la logica funcional de grpc, es decir, lo necesario
-	para que cliente y servidor hablen el mismo idioma, en concreto, dar las herramientas que se 
-	llaman al utilizar el package pb que se llama explicitamente en el main.go tanto del cliente
-	como del servidor.
+	/*
+		Se realiza el request a traves de la conexion
+		Notar que al compilar el .proto se crean structs de SeverityRequest y SeverityReply
+		en message.pb.go, dicho archivo trata con las estructuras de datos y la serializacion, es decir,
+		el ensamblado tangible de los datos para la comunicacion.
+
+		Por otro lado message_grpc.pb.gp trata con la logica funcional de grpc, es decir, lo necesario
+		para que cliente y servidor hablen el mismo idioma, en concreto, dar las herramientas que se
+		llaman al utilizar el package pb que se llama explicitamente en el main.go tanto del cliente
+		como del servidor.
 	*/
 
 	/*
-	Al comprender los parrafos anteriores se entiende que se puede revisar el struct de SeverityRequest
-	en el archivo adecuado, donde los campos definidos en el .proto son renombrados al mismo valor, pero con
-	primera letra mayuscula. 
+		Al comprender los parrafos anteriores se entiende que se puede revisar el struct de SeverityRequest
+		en el archivo adecuado, donde los campos definidos en el .proto son renombrados al mismo valor, pero con
+		primera letra mayuscula.
 
-	Al instanciar un struct, los cuales a veces funcionan como clases en go, los argumentos se pasan dentro de 
-	{} en vez de ()
+		Al instanciar un struct, los cuales a veces funcionan como clases en go, los argumentos se pasan dentro de
+		{} en vez de ()
 	*/
 
-    r, err := c.IdentifyCondition(ctx, &pb.SeverityRequest{Name : *name, Condition : *condition})
-    if err != nil{
-        log.Fatalf("could not greet: %v", err)
-    }	
-    log.Printf("Greeting: %s", r.GetMessage())
-	
+	r, err := c.IdentifyCondition(ctx, &pb.SeverityRequest{Name: *name, Condition: *condition})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+
 }
--- oms/main.go
+++ oms/main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"context"
 	"flag"
-    "fmt"
+	"fmt"
 	"log"
-    "net"
-    "time"
-    "sync"
+	"net"
+	"sync"
+	"time"
+
+	pbs "github.com/VicenteRuizA/proto_lab2/in/continente_post_oms"
+	pbc "github.com/VicenteRuizA/proto_lab2/in/oms_save_dn"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-    pbs "github.com/VicenteRuizA/proto_lab2/in/continente_post_oms"
-    pbc "github.com/VicenteRuizA/proto_lab2/in/oms_save_dn"
 )
-// notar que pbs es protocol buffer server, dado que oms es servidor en dicha communicacion
 
+// notar que pbs es protocol buffer server, dado que oms es servidor en dicha communicacion
 
 // server -------------------------------------------------------------------------------------------
 
@@ -22,13 +23,12 @@ var (
 	port = flag.Int("port", 50051, "Server port")
 )
 
-
 type server struct {
-    pbs.UnimplementedReportServer
+	pbs.UnimplementedReportServer
 }
 
 func (s *server) IdentifyCondition(ctx context.Context, in *pbs.SeverityRequest) (*pbs.SeverityReply, error) {
-    log.Printf("Received: \n Nombre: %v\n Apellido: %v\n Condicion de %v", in.GetName(), in.GetSurname(), in.GetCondition())
+	log.Printf("Received: \n Nombre: %v\n Apellido: %v\n Condicion de %v", in.GetName(), in.GetSurname(), in.GetCondition())
 
 	// Use fmt.Sprintf to format the string with variables.
 	replyMessage := fmt.Sprintf("Se ha reportado exitosamente que %s %s esta %s", in.GetName(), in.GetSurname(), in.GetCondition())
@@ -37,7 +37,7 @@ func (s *server) IdentifyCondition(ctx context.Context, in *pbs.SeverityRequest)
 }
 
 func startServer(wg *sync.WaitGroup) {
-    defer wg.Done()
+	defer wg.Done()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -49,88 +49,90 @@ func startServer(wg *sync.WaitGroup) {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
+
 // client -------------------------------------------------------------------------------------------
 
 const (
-	defaultname = "Cristiano"
-    defaultsurname = "Ronaldo"
+	defaultname      = "Cristiano"
+	defaultsurname   = "Ronaldo"
 	defaultcondition = "INFECTADO"
 )
 
 var (
-    //probar en en vms donde oms, servidor al cual pide requiest, esta en 049
-    //addr = flag.String("addr", "10.6.46.59:50051", "ip address to connect to")
+	//probar en en vms donde oms, servidor al cual pide requiest, esta en 049
+	//addr = flag.String("addr", "10.6.46.59:50051", "ip address to connect to")
 
-    //probar local
-    addr = flag.String("addr", "localhost:50051", "ip address to connect to")
-	name = flag.String("name", defaultname, "Name to report")
-	surname = flag.String("surname", defaultsurname, "Surname to report")
+	//probar local
+	addr      = flag.String("addr", "localhost:50051", "ip address to connect to")
+	name      = flag.String("name", defaultname, "Name to report")
+	surname   = flag.String("surname", defaultsurname, "Surname to report")
 	condition = flag.String("condition", defaultcondition, "Condition to report")
 )
 
-func startClient(wg *sync.WaitGroup){
-    defer wg.Done()
-    // Crear connection por el mismo puerto del listener del servidor
-    conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil{
+func startClient(wg *sync.WaitGroup) {
+	defer wg.Done()
+	// Crear connection por el mismo puerto del listener del servidor
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
 		log.Fatalf("fallo la conexion: %v", err)
 	}
 	defer conn.Close()
 
-	/* 
-	Utilizando los compilados de protocol buffer generar un cliente que
-	pida el servicio Report definido en message.proto
-	hacia el servidor con el cual se establecio la conexion conn. 
+	/*
+		Utilizando los compilados de protocol buffer generar un cliente que
+		pida el servicio Report definido en message.proto
+		hacia el servidor con el cual se establecio la conexion conn.
 	*/
 	c := pbc.NewSaveClient(conn)
 
 	/* Generar contexto
 	Los contextos nos permiten compartir informacion entre distintos
-	ambientes, en este caso el ambiente donde corre el cliente y el 
-	ambiente del servidor. El efecto de este codigo segun entiendo son 
+	ambientes, en este caso el ambiente donde corre el cliente y el
+	ambiente del servidor. El efecto de este codigo segun entiendo son
 	los tiempos de ejecuccion presentes al ejecutarse tanto el cliente
-	como el servidor. Ambos muestran su propio tiempo o context permite 
-	que ambos muestren tiempo del cliente? Sino, que informacion comparte 
+	como el servidor. Ambos muestran su propio tiempo o context permite
+	que ambos muestren tiempo del cliente? Sino, que informacion comparte
 	este context?
 	*/
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	
-	/*
-	Se realiza el request a traves de la conexion 
-	Notar que al compilar el .proto se crean structs de SeverityRequest y SeverityReply
-	en message.pb.go, dicho archivo trata con las estructuras de datos y la serializacion, es decir,
-	el ensamblado tangible de los datos para la comunicacion.
 
-	Por otro lado message_grpc.pb.gp trata con la logica funcional de grpc, es decir, lo necesario
-	para que cliente y servidor hablen el mismo idioma, en concreto, dar las herramientas que se 
-	llaman al utilizar el package pb que se llama explicitamente en el main.go tanto del cliente
-	como del servidor.
+	/*
+		Se realiza el request a traves de la conexion
+		Notar que al compilar el .proto se crean structs de SeverityRequest y SeverityReply
+		en message.pb.go, dicho archivo trata con las estructuras de datos y la serializacion, es decir,
+		el ensamblado tangible de los datos para la comunicacion.
+
+		Por otro lado message_grpc.pb.gp trata con la logica funcional de grpc, es decir, lo necesario
+		para que cliente y servidor hablen el mismo idioma, en concreto, dar las herramientas que se
+		llaman al utilizar el package pb que se llama explicitamente en el main.go tanto del cliente
+		como del servidor.
 	*/
 
 	/*
-	Al comprender los parrafos anteriores se entiende que se puede revisar el struct de SeverityRequest
-	en el archivo adecuado, donde los campos definidos en el .proto son renombrados al mismo valor, pero con
-	primera letra mayuscula. 
+		Al comprender los parrafos anteriores se entiende que se puede revisar el struct de SeverityRequest
+		en el archivo adecuado, donde los campos definidos en el .proto son renombrados al mismo valor, pero con
+		primera letra mayuscula.
 
-	Al instanciar un struct, los cuales a veces funcionan como clases en go, los argumentos se pasan dentro de 
-	{} en vez de ()
+		Al instanciar un struct, los cuales a veces funcionan como clases en go, los argumentos se pasan dentro de
+		{} en vez de ()
 	*/
 
-    r, err := c.SaveNaming(ctx, &pbc.SaveRequest{Id : *id, Name : *name, Surname : *surname})
-    if err != nil{
-        log.Fatalf("could not greet: %v", err)
-    }	
-    log.Printf("Reply from server: %s", r.GetMessage())
-	
+	r, err := c.SaveNaming(ctx, &pbc.SaveRequest{Id: *id, Name: *name, Surname: *surname})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Reply from server: %s", r.GetMessage())
+
 }
+
 // main -------------------------------------------------------------------------------------------
 func main() {
 	flag.Parse()
-    var wg sync.WaitGroup
-    wg.Add(2) // We are starting two goroutines
-    go startClient(&wg)
-    go startServer(&wg)
-    wg.Wait() // This will block until both goroutines have called Done
+	var wg sync.WaitGroup
+	wg.Add(2) // We are starting two goroutines
+	//go startClient(&wg)
+	go startServer(&wg)
+	wg.Wait() // This will block until both goroutines have called Done
 }
