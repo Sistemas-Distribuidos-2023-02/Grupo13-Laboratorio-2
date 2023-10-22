d63e6341b26331ac556e0f4e9491b716b9080857
VicenteRuizA <vicente.sd.ruiz@gmail.com>
2023-10-22 17:16:02 -0300

    seteado dn2

--- dn/dn2/main.go
+++ dn/dn2/main.go
@@ -7,7 +7,7 @@ import (
 	"log"
     "net"
 	"google.golang.org/grpc"
-    pb "github.com/VicenteRuizA/proto_lab2/in/continente_post_oms"
+    pb "github.com/VicenteRuizA/proto_lab2/dn_service"
 )
 
 
@@ -15,29 +15,34 @@ var (
 	port = flag.Int("port", 50051, "Server port")
 )
 
-type server struct {
-    pb.UnimplementedReportServer
+type saveServer struct {
+    pb.UnimplementedSaveServer
 }
 
-func (s *server) IdentifyCondition(ctx context.Context, in *pb.SeverityRequest) (*pb.SeverityReply, error) {
-    log.Printf("Received: \n Nombre: %v\n Apellido: %v\n Condicion de %v", in.GetName(), in.GetSurname(), in.GetCondition())
+func (s *saveServer) SaveNaming(ctx context.Context, in *pb.SaveRequest) (*pb.SaveReply, error) {
+    log.Printf("Received: \n ID: %v\n Nombre: %v\n Apellido: de %v", in.Id, in.GetName(), in.GetSurname())
+
 
 	// Use fmt.Sprintf to format the string with variables.
-	replyMessage := fmt.Sprintf("Se ha reportado exitosamente que %s %s esta %s", in.GetName(), in.GetSurname(), in.GetCondition())
+    replyMessage := fmt.Sprintf("Se ha reportado exitosamente ID: %s corresponde a %s %s", in.Id, in.GetName(), in.GetSurname())
 
-	return &pb.SeverityReply{Message: replyMessage}, nil
+	return &pb.SaveReply{Message: replyMessage}, nil
 }
 
-func main() {
-	flag.Parse()
+func startSaveServer(){
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterReportServer(s, &server{})
+	pb.RegisterSaveServer(s, &saveServer{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func main() {
+	flag.Parse()
+    startSaveServer()
 }
