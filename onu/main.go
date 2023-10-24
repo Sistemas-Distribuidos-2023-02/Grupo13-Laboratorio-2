a170bb8fa65f05bcc8f6f07e5952ba6808803a40
Gabriel Vergara <gabriel.vergaraa@usm.cl>
2023-10-23 22:24:37 -0300

    onu loop

--- onu/main.go
+++ onu/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 	"time"
 
 	pb "github.com/VicenteRuizA/proto_lab2/oms_service"
@@ -48,24 +51,6 @@ func connectWithRetry() (*grpc.ClientConn, error) {
 func main() {
 	flag.Parse()
 
-	/*
-		    conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-			if err != nil{
-				log.Fatalf("fallo la conexion: %v", err)
-			}
-			defer conn.Close()
-	*/
-	// fmt.Print("Solicite estado de 'Muerto' o 'Infectado': ")
-	// scanner := bufio.NewScanner(os.Stdin)
-	// scanner.Scan()
-	// condition := strings.TrimSpace(scanner.Text())
-	condition := "Muerto"
-	// Verifica que la entrada sea válida
-	if condition != "Muerto" && condition != "Infectado" {
-		fmt.Println("Entrada no válida. Debe ingresar 'Muerto' o 'Infectado'.")
-		return
-	}
-
 	conn, err := connectWithRetry()
 	if err != nil {
 		log.Fatalf("Failed to connect after 5 attempts: %v", err)
@@ -76,19 +61,33 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Hour)
 	defer cancel()
 
-	r, err := c.RequestCondition(ctx, &pb.ConditionRequest{Condition: condition})
-	if err != nil {
-		log.Fatalf("fallo en request: %v", err)
-	}
-	// Acceder a la lista de personas en la respuesta
-	personas := r.GetPersons()
+	// Bucle infinito para solicitar información continuamente
+	for {
+		fmt.Print("Solicite estado de 'Muerto' o 'Infectado': ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		condition := strings.TrimSpace(scanner.Text())
 
-	// Iterar a través de la lista de personas y hacer algo con ellas
-	for _, persona := range personas {
-		nombre := persona.GetNombre()
-		apellido := persona.GetApellido()
-		// Hacer algo con el nombre y apellido, por ejemplo, imprimirlos
-		fmt.Printf("Nombre: %s, Apellido: %s\n", nombre, apellido)
-	}
+		// Verifica que la entrada sea válida
+		if condition != "Muerto" && condition != "Infectado" {
+			fmt.Println("Entrada no válida. Debe ingresar 'Muerto' o 'Infectado'.")
+			continue // Continuar solicitando la entrada
+		}
 
+		r, err := c.RequestCondition(ctx, &pb.ConditionRequest{Condition: condition})
+		if err != nil {
+			log.Fatalf("fallo en request: %v", err)
+		}
+
+		// Acceder a la lista de personas en la respuesta
+		personas := r.GetPersons()
+
+		// Iterar a través de la lista de personas y hacer algo con ellas
+		for _, persona := range personas {
+			nombre := persona.GetNombre()
+			apellido := persona.GetApellido()
+			// Hacer algo con el nombre y apellido, por ejemplo, imprimirlos
+			fmt.Printf("%s:%s\n", nombre, apellido)
+		}
+	}
 }
